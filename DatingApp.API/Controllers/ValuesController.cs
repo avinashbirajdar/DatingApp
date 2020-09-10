1ce7205394675eb8c9230b50cba2f6c93d391964
avinashbirajdar <avinashbirajdar6728@gmail.com>
2020-09-10 21:16:28 +0530

    intial comment

---
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -0,0 +1,84 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingApp.API.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace DatingApp.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ValuesController : ControllerBase
+    {
+        private readonly DataContext context;
+        public ValuesController(DataContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var data = await context.Values.ToListAsync();
+            return Ok(data);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var data = await context.Values.FirstOrDefaultAsync(x=>x.Id==id);
+            return Ok(data);
+        }
+
+        [HttpPost]
+        public IActionResult Post()
+        {
+            return Ok();
+        }
+        [HttpPut]
+        public IActionResult Put()
+        {
+            return Ok();
+        }
+
+        [HttpDelete]
+        public IActionResult Delete()
+        {
+            return Ok();
+        }
+
+        //Api methods verbs
+        //   [HttpGet]
+        // public IActionResult Get()
+        // {
+        //     return Ok();
+        // }
+
+        // [HttpGet("{id}")]
+        // public IActionResult Get(int id)
+        // {
+        //     return Ok();
+        // }
+
+        // [HttpPost]
+        // public IActionResult Post()
+        // {
+        //     return Ok();
+        // }
+        // [HttpPut]
+        // public IActionResult Put()
+        // {
+        //     return Ok();
+        // }
+
+        // [HttpDelete]
+        // public IActionResult Delete()
+        // {
+        //     return Ok();
+        // }
+
+    }
+}
---
+++ DatingApp.API/Model/Student.cs
@@ -0,0 +1,10 @@
+namespace DatingApp.API.Model
+{
+    public class Student
+    {
+         public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
+    }
+}
